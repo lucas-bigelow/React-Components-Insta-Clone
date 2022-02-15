306513ebe01540076456805becede3f291e1922a
Lucas Bigelow <lukejbigelow@gmail.com>
2022-02-15 14:36:38 -0600

    added functionality for likes, passing the function down via props appropriately

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -9,6 +9,10 @@ const Posts = (props) => {
   return (
     <div className='posts-container-wrapper'>
       {/* Map through the posts array returning a Post component at each iteration */}
+      {posts.map(post => {
+        console.log(likePost);
+        return <Post post={post} likePost={likePost}/>
+      })}
       {/* Check the implementation of Post to see what props it requires! */}
     </div>
   );
